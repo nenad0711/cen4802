117591e0db704786f022acd7095ea9d22095eed4
nenad0711 <86927470+nenad0711@users.noreply.github.com>
2023-08-29 22:59:18 -0400

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,14 +1,16 @@
 
 
 public class Fibonacci {
+    public Fibonacci() {
+    }
 
-    public static int fibonacci_number(int n){
-        if(n<=1){
-            return n;
-        }
-        return fibonacci_number(n-1)+ fibonacci_number(n-2);
+    public static int fibonacci_number(int n) {
+        return n <= 1 ? n : fibonacci_number(n - 1) + fibonacci_number(n - 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.print("The nth term of the Fibonacci sequence is: " + fibonacci_number(10));
     }
 }
-
 
 
