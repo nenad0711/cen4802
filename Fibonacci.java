257585f7f9e3ada175e9e49da788bb97261b4f89
Nenad <nenad0711@gmail.com>
2023-08-29 21:37:08 -0400

    adding java class

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,2 +1,14 @@
+
+
 public class Fibonacci {
+
+    public static int fibonacci_number(int n){
+        if(n<=1){
+            return n;
+        }
+        return fibonacci_number(n-1)+ fibonacci_number(n-2);
+    }
 }
+
+
+
---
+++ TestFibonacci.java
@@ -0,0 +1,12 @@
+public class TestFibonacci {
+
+    public static void main(String[] args) {
+
+         System.out.print("The nth term of the Fibonacci sequence is: " + Fibonacci.fibonacci_number(10));
+
+        }
+    }
+
+
+
+
